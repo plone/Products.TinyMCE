b854f75e570724550bd0752541df84edab982489
robgietema <robgietema@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-11-02 12:23:37 +0000

    Added purge attributes to GenericSetup import
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@74864 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/TinyMCE/exportimport.py
+++ Products/TinyMCE/exportimport.py
@@ -13,76 +13,80 @@ from Products.TinyMCE.interfaces.utility import ITinyMCEResourceTypes
 
 class TinyMCESettingsXMLAdapter(XMLAdapterBase):
 
+    name = 'tinymce'
+
+    _LOGGER_ID = 'portal_tinymce'
+
     attributes = {
         'layout' : {
-            'resizing' : 'Bool',
-            'autoresize' : 'Bool',
-            'editor_width' : 'Text',
-            'editor_height' : 'Text',
-            'directionality' : 'Text',
-            'content_css' : 'Text',
-            'styles' : 'List',
-            'tablestyles' : 'List',
+            'resizing' : {'type' : 'Bool', 'default' : True},
+            'autoresize' : {'type' : 'Bool', 'default' : False},
+            'editor_width' : {'type' : 'Text', 'default' : '100%'},
+            'editor_height' : {'type' : 'Text', 'default' : '400'},
+            'directionality' : {'type' : 'Text', 'default' : 'ltr'},
+            'content_css' : {'type' : 'Text', 'default' : ''},
+            'styles' : {'type' : 'List', 'default' : 'Heading|h2\nSubheading|h3\nLiteral|pre\nDiscreet|p|discreet\nPull-quote|div|pullquote\nCall-out|p|callout\nHighlight|span|visualHighlight\nOdd row|tr|odd\nEven row|tr|even\nHeading cell|th|\nPage break (print only)|div|pageBreak\nClear floats|div|visualClear'},
+            'tablestyles' : {'type' : 'List', 'default' : 'Subdued grid|plain\nInvisible grid|invisible\nFancy listing|listing\nFancy grid listing|grid listing\nFancy vertical listing|vertical listing'},
         },
         'toolbar' : {
-            'toolbar_width' : 'Text',
-            'toolbar_external' : 'Bool',
-            'toolbar_save' : 'Bool',
-            'toolbar_cut' : 'Bool',
-            'toolbar_copy' : 'Bool',
-            'toolbar_paste' : 'Bool',
-            'toolbar_pastetext' : 'Bool',
-            'toolbar_pasteword' : 'Bool',
-            'toolbar_undo' : 'Bool',
-            'toolbar_redo' : 'Bool',
-            'toolbar_search' : 'Bool',
-            'toolbar_replace' : 'Bool',
-            'toolbar_style' : 'Bool',
-            'toolbar_bold' : 'Bool',
-            'toolbar_italic' : 'Bool',
-            'toolbar_underline' : 'Bool',
-            'toolbar_strikethrough' : 'Bool',
-            'toolbar_sub' : 'Bool',
-            'toolbar_sup' : 'Bool',
-            'toolbar_forecolor' : 'Bool',
-            'toolbar_backcolor' : 'Bool',
-            'toolbar_justifyleft' : 'Bool',
-            'toolbar_justifycenter' : 'Bool',
-            'toolbar_justifyright' : 'Bool',
-            'toolbar_justifyfull' : 'Bool',
-            'toolbar_bullist' : 'Bool',
-            'toolbar_numlist' : 'Bool',
-            'toolbar_outdent' : 'Bool',
-            'toolbar_indent' : 'Bool',
-            'toolbar_tablecontrols' : 'Bool',
-            'toolbar_link' : 'Bool',
-            'toolbar_unlink' : 'Bool',
-            'toolbar_anchor' : 'Bool',
-            'toolbar_image' : 'Bool',
-            'toolbar_media' : 'Bool',
-            'toolbar_charmap' : 'Bool',
-            'toolbar_hr' : 'Bool',
-            'toolbar_advhr' : 'Bool',
-            'toolbar_insertdate' : 'Bool',
-            'toolbar_inserttime' : 'Bool',
-            'toolbar_emotions' : 'Bool',
-            'toolbar_nonbreaking' : 'Bool',
-            'toolbar_pagebreak' : 'Bool',
-            'toolbar_print' : 'Bool',
-            'toolbar_preview' : 'Bool',
-            'toolbar_iespell' : 'Bool',
-            'toolbar_removeformat' : 'Bool',
-            'toolbar_cleanup' : 'Bool',
-            'toolbar_visualaid' : 'Bool',
-            'toolbar_visualchars' : 'Bool',
-            'toolbar_code' : 'Bool',
-            'toolbar_fullscreen' : 'Bool',
+            'toolbar_width' : {'type' : 'Text', 'default' : '440'},
+            'toolbar_external' : {'type' : 'Bool', 'default' : False},
+            'toolbar_save' : {'type' : 'Bool', 'default' : True},
+            'toolbar_cut' : {'type' : 'Bool', 'default' : False},
+            'toolbar_copy' : {'type' : 'Bool', 'default' : False},
+            'toolbar_paste' : {'type' : 'Bool', 'default' : False},
+            'toolbar_pastetext' : {'type' : 'Bool', 'default' : False},
+            'toolbar_pasteword' : {'type' : 'Bool', 'default' : False},
+            'toolbar_undo' : {'type' : 'Bool', 'default' : False},
+            'toolbar_redo' : {'type' : 'Bool', 'default' : False},
+            'toolbar_search' : {'type' : 'Bool', 'default' : False},
+            'toolbar_replace' : {'type' : 'Bool', 'default' : False},
+            'toolbar_style' : {'type' : 'Bool', 'default' : True},
+            'toolbar_bold' : {'type' : 'Bool', 'default' : True},
+            'toolbar_italic' : {'type' : 'Bool', 'default' : True},
+            'toolbar_underline' : {'type' : 'Bool', 'default' : False},
+            'toolbar_strikethrough' : {'type' : 'Bool', 'default' : False},
+            'toolbar_sub' : {'type' : 'Bool', 'default' : False},
+            'toolbar_sup' : {'type' : 'Bool', 'default' : False},
+            'toolbar_forecolor' : {'type' : 'Bool', 'default' : False},
+            'toolbar_backcolor' : {'type' : 'Bool', 'default' : False},
+            'toolbar_justifyleft' : {'type' : 'Bool', 'default' : True},
+            'toolbar_justifycenter' : {'type' : 'Bool', 'default' : True},
+            'toolbar_justifyright' : {'type' : 'Bool', 'default' : True},
+            'toolbar_justifyfull' : {'type' : 'Bool', 'default' : True},
+            'toolbar_bullist' : {'type' : 'Bool', 'default' : True},
+            'toolbar_numlist' : {'type' : 'Bool', 'default' : True},
+            'toolbar_outdent' : {'type' : 'Bool', 'default' : True},
+            'toolbar_indent' : {'type' : 'Bool', 'default' : True},
+            'toolbar_tablecontrols' : {'type' : 'Bool', 'default' : True},
+            'toolbar_link' : {'type' : 'Bool', 'default' : True},
+            'toolbar_unlink' : {'type' : 'Bool', 'default' : True},
+            'toolbar_anchor' : {'type' : 'Bool', 'default' : True},
+            'toolbar_image' : {'type' : 'Bool', 'default' : True},
+            'toolbar_media' : {'type' : 'Bool', 'default' : False},
+            'toolbar_charmap' : {'type' : 'Bool', 'default' : False},
+            'toolbar_hr' : {'type' : 'Bool', 'default' : False},
+            'toolbar_advhr' : {'type' : 'Bool', 'default' : False},
+            'toolbar_insertdate' : {'type' : 'Bool', 'default' : False},
+            'toolbar_inserttime' : {'type' : 'Bool', 'default' : False},
+            'toolbar_emotions' : {'type' : 'Bool', 'default' : False},
+            'toolbar_nonbreaking' : {'type' : 'Bool', 'default' : False},
+            'toolbar_pagebreak' : {'type' : 'Bool', 'default' : False},
+            'toolbar_print' : {'type' : 'Bool', 'default' : False},
+            'toolbar_preview' : {'type' : 'Bool', 'default' : False},
+            'toolbar_iespell' : {'type' : 'Bool', 'default' : False},
+            'toolbar_removeformat' : {'type' : 'Bool', 'default' : False},
+            'toolbar_cleanup' : {'type' : 'Bool', 'default' : False},
+            'toolbar_visualaid' : {'type' : 'Bool', 'default' : False},
+            'toolbar_visualchars' : {'type' : 'Bool', 'default' : False},
+            'toolbar_code' : {'type' : 'Bool', 'default' : True},
+            'toolbar_fullscreen' : {'type' : 'Bool', 'default' : True},
         },
         'resourcetypes' : {
-            'containsobjects' : 'List',
-            'containsanchors' : 'List',
-            'linkable' : 'List',
-            'imageobjects' : 'List',
+            'containsobjects' : {'type' : 'List', 'default' : 'ATFolder\nATBTreeFolder\nPlone Site'},
+            'containsanchors' : {'type' : 'List', 'default' : 'ATEvent\nATNewsItem\nATDocument\nATRelativePathCriterion'},
+            'linkable' : {'type' : 'List', 'default' : 'ATTopic\nATEvent\nATFile\nATFolder\nATImage\nATBTreeFolder\nATNewsItem\nATDocument'},
+            'imageobjects' : {'type' : 'List', 'default' : 'ATImage'},
         }
     }
 
@@ -101,16 +105,16 @@ class TinyMCESettingsXMLAdapter(XMLAdapterBase):
                 fieldnode = self._doc.createElement(field)
                 fieldvalue = getattr(self.context, field)
 
-                if category[field] == 'Bool':
+                if category[field]['type'] == 'Bool':
                     fieldnode.setAttribute('value', unicode(bool(fieldvalue)))
-                elif category[field] == 'Text':
+                elif category[field]['type'] == 'Text':
 
                     # Check for NoneType
                     if fieldvalue:
                         fieldnode.setAttribute('value', fieldvalue)
                     else:
                         fieldnode.setAttribute('value', '')
-                elif category[field] == 'List':
+                elif category[field]['type'] == 'List':
                     for value in fieldvalue.split('\n'):
                         if value:
                             child = self._doc.createElement('element')
@@ -123,7 +127,23 @@ class TinyMCESettingsXMLAdapter(XMLAdapterBase):
     def _importNode(self, node):
         """Import the object from the DOM node"""
 
-        pass
+        if self.environ.shouldPurge():
+            self._purgeAttributes()
+
+        self._logger.info('TinyMCE Settings imported.')
+
+    def _purgeAttributes(self):
+        """Purge current attributes"""
+
+        # Loop through categories
+        for key in self.attributes.keys():
+            category = self.attributes[key]
+
+            # Loop through fields in category
+            for field in category.keys():
+                fieldvalue = getattr(self.context, field)
+
+                fieldvalue = category[field]['default']
 
 def importTinyMCESettings(context):
     """Import TinyMCE Settings"""
