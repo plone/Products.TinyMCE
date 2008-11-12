a1ce028ddac763e8bebcea4f80c4e57c88330d61
Martijn Jacobs <martijn@fourdigits.nl>
2008-11-12 15:46:13 +0000

    Removed extension for tinmymce-output-html mimetype (not necessary) Changed getToolByName calls to getUtility in setuphandlers
    svn path=/Products.TinyMCE/trunk/; revision=75640

--- Products/TinyMCE/mimetypes/mimetypes.py
+++ Products/TinyMCE/mimetypes/mimetypes.py
@@ -9,5 +9,4 @@ class text_tinymce_output_html(MimeTypeItem):
     __implements__ = MimeTypeItem.__implements__
     __name__   = "TinyMCE Output HTML"
     mimetypes  = ('text/x-tinymce-output-html',)
-    extensions = ('html',)
     binary     = 0
