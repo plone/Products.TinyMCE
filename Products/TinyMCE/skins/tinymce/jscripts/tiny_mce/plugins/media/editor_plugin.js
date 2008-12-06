9b2976dda6555ee8c0110a7dcea955c6df730d7c
robgietema <robgietema@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-12-06 18:32:39 +0000

    Fixed insert media dialog
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@76996 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/TinyMCE/skins/tinymce/jscripts/tiny_mce/plugins/media/editor_plugin.js
+++ Products/TinyMCE/skins/tinymce/jscripts/tiny_mce/plugins/media/editor_plugin.js
@@ -28,8 +28,8 @@
 			ed.addCommand('mceMedia', function() {
 				ed.windowManager.open({
 					file : url + '/media.htm',
-					width : 430 + parseInt(ed.getLang('media.delta_width', 0)),
-					height : 470 + parseInt(ed.getLang('media.delta_height', 0)),
+					width : 600 + parseInt(ed.getLang('media.delta_width', 0)),
+					height : 520 + parseInt(ed.getLang('media.delta_height', 0)),
 					inline : 1
 				}, {
 					plugin_url : url
