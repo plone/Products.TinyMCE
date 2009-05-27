d987507bd068c24c9c17d5d74d0cff11079483c6
robgietema <robgietema@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-05-27 07:51:17 +0000

    Update to TinyMCE 3.2.4.1.
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@86859 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/TinyMCE/skins/tinymce/jscripts/tiny_mce/plugins/paste/js/pasteword.js
+++ Products/TinyMCE/skins/tinymce/jscripts/tiny_mce/plugins/paste/js/pasteword.js
@@ -1,38 +1,39 @@
 tinyMCEPopup.requireLangPack();
 
-function saveContent() {
-	var html = document.getElementById("frmData").contentWindow.document.body.innerHTML;
+var PasteWordDialog = {
+	init : function() {
+		var ed = tinyMCEPopup.editor, el = document.getElementById('iframecontainer'), ifr, doc, css, cssHTML = '';
 
-	if (html == ''){
+		// Create iframe
+		el.innerHTML = '<iframe id="iframe" src="javascript:\'\';" class="input-border" frameBorder="0"></iframe>';
+		ifr = document.getElementById('iframe');
+		doc = ifr.contentWindow.document;
+
+		// Force absolute CSS urls
+		css = [ed.baseURI.toAbsolute("themes/" + ed.settings.theme + "/skins/" + ed.settings.skin + "/content.css")];
+		css = css.concat(tinymce.explode(ed.settings.content_css) || []);
+		tinymce.each(css, function(u) {
+			cssHTML += '<link href="' + ed.documentBaseURI.toAbsolute(u) + '" rel="stylesheet" type="text/css" />';
+		});
+
+		// Write content into iframe
+		doc.open();
+		doc.write('<html><head>' + cssHTML + '</head><body class="mceContentBody" spellcheck="false"></body></html>');
+		doc.close();
+
+		doc.designMode = 'on';
+
+		window.setTimeout(function() {
+			ifr.contentWindow.focus();
+		}, 10);
+	},
+
+	insert : function() {
+		var h = document.getElementById('iframe').contentWindow.document.body.innerHTML;
+
+		tinyMCEPopup.editor.execCommand('mceInsertClipboardContent', false, h);
 		tinyMCEPopup.close();
-		return false;
-	}
+	},
+};
 
-	tinyMCEPopup.execCommand('mcePasteWord', false, html);
-	tinyMCEPopup.close();
-}
-
-function onLoadInit() {
-	tinyMCEPopup.resizeToInnerSize();
-
-	// Fix for endless reloading in FF
-	window.setTimeout(createIFrame, 10);
-}
-
-function createIFrame() {
-	document.getElementById('iframecontainer').innerHTML = '<iframe id="frmData" name="frmData" class="input-border" src="blank.htm" height="261" width="398" frameborder="0" style="background-color:#FFFFFF; width:100%;" dir="ltr" wrap="soft"></iframe>';
-}
-
-var wHeight=0, wWidth=0, owHeight=0, owWidth=0;
-
-function initIframe(doc) {
-	var dir = tinyMCEPopup.editor.settings.directionality;
-
-	doc.body.dir = dir;
-
-	// Remove Gecko spellchecking
-	if (tinymce.isGecko)
-		doc.body.spellcheck = tinyMCEPopup.getParam("gecko_spellcheck");
-}
-
-tinyMCEPopup.onInit.add(onLoadInit);
+tinyMCEPopup.onInit.add(PasteWordDialog.init, PasteWordDialog);
