7fc2598ea043c41866bad2cc38a5e1ddedbac4b7
robgietema <robgietema@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-07 13:51:55 +0000

    Fix for issue #59.
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@93953 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/TinyMCE/skins/tinymce/jscripts/tiny_mce/plugins/save/editor_plugin.js
+++ Products/TinyMCE/skins/tinymce/jscripts/tiny_mce/plugins/save/editor_plugin.js
@@ -6,117 +6,120 @@
  */
 
 (function() {
-	tinymce.create('tinymce.plugins.Save', {
-		init : function(ed, url) {
-			var t = this;
+    tinymce.create('tinymce.plugins.Save', {
+        init : function(ed, url) {
+            var t = this;
 
-			t.editor = ed;
+            t.editor = ed;
 
-			// Register commands
-			ed.addCommand('mceSave', function() {
-				tinymce.util.XHR.send({
-					url : ed.settings.document_url + '/tinymce-save',
-					content_type : "application/x-www-form-urlencoded",
-					type : "POST",
-					data : "text=" + encodeURIComponent(ed.getContent()) + "&fieldname=" + ed.id,
-					success : function( data, req, o ) {
-						if (!tinymce.isIE) {
-							tinymce.DOM.addClass(tinymce.DOM.get(ed.id + '_tbl'), 'mceEditorSave');
-							window.setTimeout(function() {
-								tinymce.DOM.removeClass(tinymce.DOM.get(ed.id + '_tbl'), 'mceEditorSave');
-								tinymce.DOM.removeClass(tinymce.DOM.get(ed.id + '_tbl'), 'mceEditorFocus');
-								tinymce.DOM.addClass(tinymce.DOM.get(ed.id + '_tbl'), 'mceEditorFocus');
-							}, 500);
-						}
-						tinymce.DOM.add(document.body, 'div', {id: 'mceSaveMessage', 'class' : 'mceSaveMessage'}, 'Document saved.');
-							window.setTimeout(function () {
-							tinymce.DOM.remove('mceSaveMessage');
-						}, 1000);
-					}
-				});
-			});
-				
-			ed.addCommand('mceCancel', t._cancel);
+            // Register commands
+            ed.addCommand('mceSave', function() {
+                var id = ed.id;
+                if (ed.id == 'mce_fullscreen') {
+                    id = ed.settings.fullscreen_editor_id;
+                }
+                tinymce.util.XHR.send({
+                    url : ed.settings.document_url + '/tinymce-save',
+                    content_type : "application/x-www-form-urlencoded",
+                    type : "POST",
+                    data : "text=" + encodeURIComponent(ed.getContent()) + "&fieldname=" + id,
+                    success : function( data, req, o ) {
+                        if (!tinymce.isIE) {
+                            tinymce.DOM.addClass(tinymce.DOM.get(ed.id + '_tbl'), 'mceEditorSave');
+                            window.setTimeout(function() {
+                                tinymce.DOM.removeClass(tinymce.DOM.get(ed.id + '_tbl'), 'mceEditorSave');
+                                tinymce.DOM.removeClass(tinymce.DOM.get(ed.id + '_tbl'), 'mceEditorFocus');
+                                tinymce.DOM.addClass(tinymce.DOM.get(ed.id + '_tbl'), 'mceEditorFocus');
+                            }, 500);
+                        }
+                        tinymce.DOM.add(document.body, 'div', {id: 'mceSaveMessage', 'class' : 'mceSaveMessage'}, 'Document saved.');
+                        window.setTimeout(function () {
+                            tinymce.DOM.remove('mceSaveMessage');
+                        }, 1000);
+                    }
+                });
+            });
 
-			// Register buttons
-			ed.addButton('save', {title : 'save.save_desc', cmd : 'mceSave'});
-			ed.addButton('cancel', {title : 'save.cancel_desc', cmd : 'mceCancel'});
+            ed.addCommand('mceCancel', t._cancel);
 
-			// ed.onNodeChange.add(t._nodeChange, t);
-			ed.addShortcut('ctrl+s', ed.getLang('save.save_desc'), 'mceSave');
-		},
+            // Register buttons
+            ed.addButton('save', {title : 'save.save_desc', cmd : 'mceSave'});
+            ed.addButton('cancel', {title : 'save.cancel_desc', cmd : 'mceCancel'});
 
-		getInfo : function() {
-			return {
-				longname : 'Save',
-				author : 'Moxiecode Systems AB',
-				authorurl : 'http://tinymce.moxiecode.com',
-				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/save',
-				version : tinymce.majorVersion + "." + tinymce.minorVersion
-			};
-		},
+            ed.onNodeChange.add(t._nodeChange, t);
+            ed.addShortcut('ctrl+s', ed.getLang('save.save_desc'), 'mceSave');
+        },
 
-		// Private methods
+        getInfo : function() {
+            return {
+                longname : 'Save',
+                author : 'Moxiecode Systems AB',
+                authorurl : 'http://tinymce.moxiecode.com',
+                infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/save',
+                version : tinymce.majorVersion + "." + tinymce.minorVersion
+            };
+        },
 
-		_nodeChange : function(ed, cm, n) {
-			var ed = this.editor;
+        // Private methods
+        _nodeChange : function(ed, cm, n) {
+            var ed = this.editor;
 
-			if (ed.getParam('save_enablewhendirty')) {
-				cm.setDisabled('save', !ed.isDirty());
-				cm.setDisabled('cancel', !ed.isDirty());
-			}
-		},
+            if (ed.getParam('save_enablewhendirty')) {
+                cm.setDisabled('save', !ed.isDirty());
+                cm.setDisabled('cancel', !ed.isDirty());
+            }
+        },
 
-		// Private methods
+        // Private methods
 
-		_save : function() {
-			var ed = this.editor, formObj, os, i, elementId;
+        _save : function() {
+            var ed = this.editor, formObj, os, i, elementId;
 
-			formObj = tinymce.DOM.get(ed.id).form || tinymce.DOM.getParent(ed.id, 'form');
+            formObj = tinymce.DOM.get(ed.id).form || tinymce.DOM.getParent(ed.id, 'form');
 
-			if (ed.getParam("save_enablewhendirty") && !ed.isDirty())
-				return;
+            if (ed.getParam("save_enablewhendirty") && !ed.isDirty())
+                return;
 
-			tinyMCE.triggerSave();
+            tinyMCE.triggerSave();
 
-			// Use callback instead
-			if (os = ed.getParam("save_onsavecallback")) {
-				if (ed.execCallback('save_onsavecallback', ed)) {
-					ed.startContent = tinymce.trim(ed.getContent({format : 'raw'}));
-					ed.nodeChanged();
-				}
+            // Use callback instead
+            if (os = ed.getParam("save_onsavecallback")) {
+                if (ed.execCallback('save_onsavecallback', ed)) {
+                    ed.startContent = tinymce.trim(ed.getContent({format : 'raw'}));
+                    ed.nodeChanged();
+                }
 
-				return;
-			}
+                return;
+            }
 
-			if (formObj) {
-				ed.isNotDirty = true;
+            if (formObj) {
+                ed.isNotDirty = true;
 
-				if (formObj.onsubmit == null || formObj.onsubmit() != false)
-					formObj.submit();
+                if (formObj.onsubmit == null || formObj.onsubmit() != false)
+                    formObj.submit();
 
-				ed.nodeChanged();
-			} else
-				ed.windowManager.alert("Error: No form element found.");
+                ed.nodeChanged();
+            } else
+                ed.windowManager.alert("Error: No form element found.");
 
-			return true;
-		},
+            return true;
+        },
 
-		_cancel : function() {
-			var ed = this.editor, os, h = tinymce.trim(ed.startContent);
+        _cancel : function() {
+            var ed = this.editor, os, h = tinymce.trim(ed.startContent);
 
-			// Use callback instead
-			if (os = ed.getParam("save_oncancelcallback")) {
-				ed.execCallback('save_oncancelcallback', ed);
-				return;
-			}
+            // Use callback instead
+            if (os = ed.getParam("save_oncancelcallback")) {
+                ed.execCallback('save_oncancelcallback', ed);
+                return;
+            }
 
-			ed.setContent(h);
-			ed.undoManager.clear();
-			ed.nodeChanged();
-		}
-	});
+            ed.setContent(h);
+            ed.undoManager.clear();
+            ed.nodeChanged();
+        }
+    });
 
-	// Register plugin
-	tinymce.PluginManager.add('save', tinymce.plugins.Save);
+    // Register plugin
+    tinymce.PluginManager.add('save', tinymce.plugins.Save);
 })();
