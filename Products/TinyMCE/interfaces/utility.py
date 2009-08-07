7be0145dad7d42e55d853a9fac790a5f3c56ba62
robgietema <robgietema@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-07 18:11:05 +0000

    Updated translations.
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@94022 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/TinyMCE/interfaces/utility.py
+++ Products/TinyMCE/interfaces/utility.py
@@ -87,307 +87,256 @@ class ITinyMCEToolbar(Interface):
 
 	toolbar_save = schema.Bool(
 		title=_(u'label_toolbar_save', default=u"Save"),
-		description=_(u"help_toolbar_save", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_cut = schema.Bool(
 		title=_(u'label_toolbar_cut', default=u"Cut"),
-		description=_(u"help_toolbar_cut", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_copy = schema.Bool(
 		title=_(u'label_toolbar_copy', default=u"Copy"),
-		description=_(u"help_toolbar_copy", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_paste = schema.Bool(
 		title=_(u'label_toolbar_paste', default=u"Paste"),
-		description=_(u"help_toolbar_paste", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_pastetext = schema.Bool(
 		title=_(u'label_toolbar_pastetext', default=u"Paste as Plain Text"),
-		description=_(u"help_toolbar_pastetext", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_pasteword = schema.Bool(
 		title=_(u'label_toolbar_pasteword', default=u"Paste from Word"),
-		description=_(u"help_toolbar_pasteword", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_undo = schema.Bool(
 		title=_(u'label_toolbar_undo', default=u"Undo"),
-		description=_(u"help_toolbar_undo", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_redo = schema.Bool(
 		title=_(u'label_toolbar_redo', default=u"Redo"),
-		description=_(u"help_toolbar_redo", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_search = schema.Bool(
 		title=_(u'label_toolbar_search', default=u"Find"),
-		description=_(u"help_toolbar_search", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_replace = schema.Bool(
 		title=_(u'label_toolbar_replace', default=u"Find/Replace"),
-		description=_(u"help_toolbar_replace", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_style = schema.Bool(
 		title=_(u'label_toolbar_style', default=u"Select Style"),
-		description=_(u"help_toolbar_style", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_bold = schema.Bool(
 		title=_(u'label_toolbar_bold', default=u"Bold"),
-		description=_(u"help_toolbar_bold", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_italic = schema.Bool(
 		title=_(u'label_toolbar_italic', default=u"Italic"),
-		description=_(u"help_toolbar_italic", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_underline = schema.Bool(
 		title=_(u'label_toolbar_underline', default=u"Underline"),
-		description=_(u"help_toolbar_underline", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_strikethrough = schema.Bool(
 		title=_(u'label_toolbar_strikethrough', default=u"Strikethrough"),
-		description=_(u"help_toolbar_strikethrough", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_sub = schema.Bool(
 		title=_(u'label_toolbar_sub', default=u"Subscript"),
-		description=_(u"help_toolbar_sub", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_sup = schema.Bool(
 		title=_(u'label_toolbar_sup', default=u"Superscript"),
-		description=_(u"help_toolbar_sup", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_forecolor = schema.Bool(
 		title=_(u'label_toolbar_forecolor', default=u"Forecolor"),
-		description=_(u"help_toolbar_forecolor", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_backcolor = schema.Bool(
 		title=_(u'label_toolbar_backcolor', default=u"Backcolor"),
-		description=_(u"help_toolbar_backcolor", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_justifyleft = schema.Bool(
 		title=_(u'label_toolbar_justifyleft', default=u"Align left"),
-		description=_(u"help_toolbar_justifyleft", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_justifycenter = schema.Bool(
 		title=_(u'label_toolbar_justifycenter', default=u"Align center"),
-		description=_(u"help_toolbar_justifycenter", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_justifyright = schema.Bool(
 		title=_(u'label_toolbar_justifyright', default=u"Align right"),
-		description=_(u"help_toolbar_justifyright", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_justifyfull = schema.Bool(
 		title=_(u'label_toolbar_justifyfull', default=u"Align full"),
-		description=_(u"help_toolbar_justifyfull", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_bullist = schema.Bool(
 		title=_(u'label_toolbar_bullist', default=u"Unordered list"),
-		description=_(u"help_toolbar_bullist", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_numlist = schema.Bool(
 		title=_(u'label_toolbar_numlist', default=u"Ordered list"),
-		description=_(u"help_toolbar_numlist", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_outdent = schema.Bool(
 		title=_(u'label_toolbar_outdent', default=u"Outdent"),
-		description=_(u"help_toolbar_outdent", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_indent = schema.Bool(
 		title=_(u'label_toolbar_indent', default=u"Indent"),
-		description=_(u"help_toolbar_indent", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_tablecontrols = schema.Bool(
 		title=_(u'label_toolbar_tablecontrols', default=u"Table controls"),
-		description=_(u"help_toolbar_tablecontrols", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_link = schema.Bool(
 		title=_(u'label_toolbar_link', default=u"Insert/edit link"),
-		description=_(u"help_toolbar_link", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_unlink = schema.Bool(
 		title=_(u'label_toolbar_unlink', default=u"Unlink"),
-		description=_(u"help_toolbar_unlink", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_anchor = schema.Bool(
 		title=_(u'label_toolbar_anchor', default=u"Insert/edit anchor"),
-		description=_(u"help_toolbar_anchor", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_image = schema.Bool(
 		title=_(u'label_toolbar_image', default=u"Insert/edit image"),
-		description=_(u"help_toolbar_image", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_media = schema.Bool(
 		title=_(u'label_toolbar_media', default=u"Insert/edit media"),
-		description=_(u"help_toolbar_media", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_charmap = schema.Bool(
 		title=_(u'label_toolbar_charmap', default=u"Insert custom character"),
-		description=_(u"help_toolbar_charmap", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_hr = schema.Bool(
 		title=_(u'label_toolbar_hr', default=u"Insert horizontal ruler"),
-		description=_(u"help_toolbar_hr", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_advhr = schema.Bool(
 		title=_(u'label_toolbar_advhr', default=u"Insert advanced horizontal ruler"),
-		description=_(u"help_toolbar_advhr", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_insertdate = schema.Bool(
 		title=_(u'label_toolbar_insertdate', default=u"Insert date"),
-		description=_(u"help_toolbar_insertdate", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_inserttime = schema.Bool(
 		title=_(u'label_toolbar_inserttime', default=u"Insert time"),
-		description=_(u"help_toolbar_inserttime", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_emotions = schema.Bool(
 		title=_(u'label_toolbar_emotions', default=u"Emotions"),
-		description=_(u"help_toolbar_emotions", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_nonbreaking = schema.Bool(
 		title=_(u'label_toolbar_nonbreaking', default=u"Insert non-breaking space character"),
-		description=_(u"help_toolbar_nonbreaking", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_pagebreak = schema.Bool(
 		title=_(u'label_toolbar_pagebreak', default=u"Insert page break"),
-		description=_(u"help_toolbar_pagebreak", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_print = schema.Bool(
 		title=_(u'label_toolbar_print', default=u"Print"),
-		description=_(u"help_toolbar_print", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_preview = schema.Bool(
 		title=_(u'label_toolbar_preview', default=u"Preview"),
-		description=_(u"help_toolbar_preview", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_iespell = schema.Bool(
 		title=_(u'label_toolbar_iespell', default=u"Spellchecker"),
-		description=_(u"help_toolbar_iespell", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_removeformat = schema.Bool(
 		title=_(u'label_toolbar_removeformat', default=u"Remove formatting"),
-		description=_(u"help_toolbar_removeformat", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_cleanup = schema.Bool(
 		title=_(u'label_toolbar_cleanup', default=u"Cleanup messy code"),
-		description=_(u"help_toolbar_cleanup", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_visualaid = schema.Bool(
 		title=_(u'label_toolbar_visualaid', default=u"Toggle guidelines/invisible objects"),
-		description=_(u"help_toolbar_visualaid", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_visualchars = schema.Bool(
 		title=_(u'label_toolbar_visualchars', default=u"Visual control characters on/off"),
-		description=_(u"help_toolbar_visualchars", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_attribs = schema.Bool(
 		title=_(u'label_toolbar_attribs', default=u"Insert/edit attributes"),
-		description=_(u"help_toolbar_attribs", default=u""),
 		default=False,
 		required=False)
 
 	toolbar_code = schema.Bool(
 		title=_(u'label_toolbar_code', default=u"Edit HTML Source"),
-		description=_(u"help_toolbar_code", default=u""),
 		default=True,
 		required=False)
 
 	toolbar_fullscreen = schema.Bool(
 		title=_(u'label_toolbar_fullscreen', default=u"Toggle fullscreen mode"),
-		description=_(u"help_toolbar_fullscreen", default=u""),
 		default=True,
 		required=False)
 
