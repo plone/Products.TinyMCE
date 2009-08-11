2c79afea8324be0393ec9416eb46b586f74d6c4d
robgietema <robgietema@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-11 07:55:29 +0000

    Updated info on link and image plugins.
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@94379 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/TinyMCE/skins/tinymce/plugins/ploneimage/editor_plugin.js
+++ Products/TinyMCE/skins/tinymce/plugins/ploneimage/editor_plugin.js
@@ -1,47 +1,46 @@
 /**
- * $Id: editor_plugin_src.js 677 2008-03-07 13:52:41Z spocke $
+ * Plone image plugin based on advimage plugin.
  *
- * @author Moxiecode
- * @copyright Copyright © 2004-2008, Moxiecode Systems AB, All rights reserved.
+ * @author Rob Gietema
  */
 
 (function() {
-	tinymce.create('tinymce.plugins.PloneImagePlugin', {
-		init : function(ed, url) {
-			// Register commands
-			ed.addCommand('mcePloneImage', function() {
-				// Internal image object like a flash placeholder
-				if (ed.dom.getAttrib(ed.selection.getNode(), 'class').indexOf('mceItem') != -1)
-					return;
+    tinymce.create('tinymce.plugins.PloneImagePlugin', {
+        init : function(ed, url) {
+            // Register commands
+            ed.addCommand('mcePloneImage', function() {
+                // Internal image object like a flash placeholder
+                if (ed.dom.getAttrib(ed.selection.getNode(), 'class').indexOf('mceItem') != -1)
+                    return;
 
-				ed.windowManager.open({
-					file : url + '/ploneimage.htm',
-					width : 820 + parseInt(ed.getLang('ploneimage.delta_width', 0)),
-					height : 470 + parseInt(ed.getLang('ploneimage.delta_height', 0)),
-					inline : 1
-				}, {
-					plugin_url : url
-				});
-			});
+                ed.windowManager.open({
+                    file : url + '/ploneimage.htm',
+                    width : 820 + parseInt(ed.getLang('ploneimage.delta_width', 0)),
+                    height : 470 + parseInt(ed.getLang('ploneimage.delta_height', 0)),
+                    inline : 1
+                }, {
+                    plugin_url : url
+                });
+            });
 
-			// Register buttons
-			ed.addButton('image', {
-				title : 'ploneimage.image_desc',
-				cmd : 'mcePloneImage'
-			});
-		},
+            // Register buttons
+            ed.addButton('image', {
+                title : 'ploneimage.image_desc',
+                cmd : 'mcePloneImage'
+            });
+        },
 
-		getInfo : function() {
-			return {
-				longname : 'Plone image',
-				author : 'Rob Gietema',
-				authorurl : 'http://plone.org',
-				infourl : 'http://plone.org/products/tinymce',
-				version : tinymce.majorVersion + "." + tinymce.minorVersion
-			};
-		}
-	});
+        getInfo : function() {
+            return {
+                longname : 'Plone image',
+                author : 'Rob Gietema',
+                authorurl : 'http://plone.org',
+                infourl : 'http://plone.org/products/tinymce',
+                version : tinymce.majorVersion + "." + tinymce.minorVersion
+            };
+        }
+    });
 
-	// Register plugin
-	tinymce.PluginManager.add('ploneimage', tinymce.plugins.PloneImagePlugin);
+    // Register plugin
+    tinymce.PluginManager.add('ploneimage', tinymce.plugins.PloneImagePlugin);
 })();
