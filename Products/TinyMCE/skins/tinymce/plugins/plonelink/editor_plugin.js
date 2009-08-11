82b9685a434ac5690cf1a07b64bd3a4839853e41
Rob Gietema <rob@fourdigits.nl>
2009-08-11 07:55:29 +0000

    Updated info on link and image plugins.
    svn path=/Products.TinyMCE/trunk/; revision=94379

--- Products/TinyMCE/skins/tinymce/plugins/plonelink/editor_plugin.js
+++ Products/TinyMCE/skins/tinymce/plugins/plonelink/editor_plugin.js
@@ -5,53 +5,53 @@
  */
 
 (function() {
-	tinymce.create('tinymce.plugins.PloneLinkPlugin', {
-		init : function(ed, url) {
-			this.editor = ed;
+    tinymce.create('tinymce.plugins.PloneLinkPlugin', {
+        init : function(ed, url) {
+            this.editor = ed;
 
-			// Register commands
-			ed.addCommand('mcePloneLink', function() {
-				var se = ed.selection;
+            // Register commands
+            ed.addCommand('mcePloneLink', function() {
+                var se = ed.selection;
 
-				// No selection and not in link
-				if (se.isCollapsed() && !ed.dom.getParent(se.getNode(), 'A'))
-					return;
+                // No selection and not in link
+                if (se.isCollapsed() && !ed.dom.getParent(se.getNode(), 'A'))
+                    return;
 
-				ed.windowManager.open({
-					file : url + '/plonelink.htm',
-					width : 820 + parseInt(ed.getLang('plonelink.delta_width', 0)),
-					height : 470 + parseInt(ed.getLang('plonelink.delta_height', 0)),
-					inline : 1
-				}, {
-					plugin_url : url
-				});
-			});
+                ed.windowManager.open({
+                    file : url + '/plonelink.htm',
+                    width : 820 + parseInt(ed.getLang('plonelink.delta_width', 0)),
+                    height : 470 + parseInt(ed.getLang('plonelink.delta_height', 0)),
+                    inline : 1
+                }, {
+                    plugin_url : url
+                });
+            });
 
-			// Register buttons
-			ed.addButton('link', {
-				title : 'plonelink.link_desc',
-				cmd : 'mcePloneLink'
-			});
+            // Register buttons
+            ed.addButton('link', {
+                title : 'plonelink.link_desc',
+                cmd : 'mcePloneLink'
+            });
 
-			ed.addShortcut('ctrl+k', 'plonelink.advlink_desc', 'mcePloneLink');
+            ed.addShortcut('ctrl+k', 'plonelink.advlink_desc', 'mcePloneLink');
 
-			ed.onNodeChange.add(function(ed, cm, n, co) {
-				cm.setDisabled('link', co && n.nodeName != 'A');
-				cm.setActive('link', n.nodeName == 'A' && !n.name);
-			});
-		},
+            ed.onNodeChange.add(function(ed, cm, n, co) {
+                cm.setDisabled('link', co && n.nodeName != 'A');
+                cm.setActive('link', n.nodeName == 'A' && !n.name);
+            });
+        },
 
-		getInfo : function() {
-			return {
-				longname : 'Plone link',
-				author : 'Rob Gietema',
-				authorurl : 'http://plone.org',
-				infourl : 'http://plone.org/products/tinymce',
-				version : tinymce.majorVersion + "." + tinymce.minorVersion
-			};
-		}
-	});
+        getInfo : function() {
+            return {
+                longname : 'Plone link',
+                author : 'Rob Gietema',
+                authorurl : 'http://plone.org',
+                infourl : 'http://plone.org/products/tinymce',
+                version : tinymce.majorVersion + "." + tinymce.minorVersion
+            };
+        }
+    });
 
-	// Register plugin
-	tinymce.PluginManager.add('plonelink', tinymce.plugins.PloneLinkPlugin);
+    // Register plugin
+    tinymce.PluginManager.add('plonelink', tinymce.plugins.PloneLinkPlugin);
 })();
