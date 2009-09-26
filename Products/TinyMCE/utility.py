b1fdcd9b6e51c156ab498d229447426f8bc8cb0f
Rob Gietema <rob@fourdigits.nl>
2009-09-26 11:30:33 +0000

    Changed condition for Javascript includes.
    svn path=/Products.TinyMCE/trunk/; revision=98270

--- Products/TinyMCE/utility.py
+++ Products/TinyMCE/utility.py
@@ -119,41 +119,6 @@ class TinyMCE(SimpleItem):
     imageobjects = FieldProperty(ITinyMCEResourceTypes['imageobjects'])
     customplugins = FieldProperty(ITinyMCEResourceTypes['customplugins'])
 
-    security.declareProtected('View', 'isTinyMCEEnabled')
-    def isTinyMCEEnabled(self, useragent='', allowAnonymous=False, REQUEST=None, context=None, fieldName=None):
-        if not REQUEST:
-            REQUEST = self.REQUEST
-        def numerics(s):
-            """Convert a string into a tuple of all digit sequences"""
-            seq = ['']
-            for c in s:
-                if c.isdigit():
-                    seq[-1] = seq[-1] + c
-                elif seq[-1]:
-                    seq.append('')
-            return tuple([ int(val) for val in seq if val])
-
-        # First check whether the user actually wants tinymce
-        pm = getToolByName(self, 'portal_membership')
-        if pm.isAnonymousUser() and not allowAnonymous:
-            return False
-
-        user = pm.getAuthenticatedMember()
-        if not pm.isAnonymousUser():
-            editor = user.getProperty('wysiwyg_editor')
-            if editor and editor.lower() != 'tinymce':
-                return False
-
-        # Then check whether the current content allows html
-        if context is not None and fieldName and hasattr(context, 'getWrappedField'):
-            field = context.getWrappedField(fieldName)
-            if field:
-                allowedTypes = getattr(field, 'allowable_content_types', None)
-                if allowedTypes is not None and not 'text/html' in [t.lower() for t in allowedTypes]:
-                    return False
-
-        return True
-
     def getImageScales(self, primary_field=None):
         """Return the image sizes for the drawer"""
         if primary_field is None:
