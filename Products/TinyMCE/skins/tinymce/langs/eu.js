d9066fa70c212f38f6c9881bb1f44b6b4292ea5d
erral <erral@localhost>
2009-10-15 15:45:39 +0000

    added basque translation for TinyMCE
    svn path=/Products.TinyMCE/trunk/; revision=99614

---
+++ Products/TinyMCE/skins/tinymce/langs/eu.js
@@ -0,0 +1,154 @@
+tinyMCE.addI18n({eu:{
+common:{
+edit_confirm:"Testu kutxa honentzat WYSIWYG modua erabili nahi duzu?",
+apply:"Gorde",
+insert:"Sartu",
+update:"Eguneratu",
+cancel:"Utzi",
+close:"Itxi",
+browse:"Arakatu",
+class_name:"Klasea",
+not_set:"-- Bat ere ez --",
+clipboard_msg:"Kopiatu/Ebaki/Itsatsi ez dago Mozilla eta Firefox nabigatzaileetan. Honen inguruko informazio gehiago lortu nahi duzu?",
+clipboard_no_support:"Zure nabigatzaileak ezin ditu arbelaren funtzioak erabili. Erabili teklatu bidezko laster-teklak.",
+popup_blocked:"Barkatu, pop-up leihoak blokeatuta dituzu eta horrek aplikazioaren atal batzuk ez ibiltzea dakar. Blokeo hori desgaitu egin behar duzu funtzio guztiak erabiltzeko.",
+invalid_data:"Errorea: Gorriz markatutako testua ez da baliozko.",
+more_colors:"Kolore gehiago"
+},
+contextmenu:{
+align:"Alineazioa",
+left:"Ezkerrean",
+center:"Erdian",
+right:"Eskuinean",
+full:"Justifikatuta"
+},
+insertdatetime:{
+date_fmt:"%Y-%m-%d",
+time_fmt:"%H:%M:%S",
+insertdate_desc:"Data sartu",
+inserttime_desc:"Ordua sartu",
+months_long:"Urtarrila,Otsaila,Martxoa,Apirila,Maiatza,Ekaina,Uztaila,Abuztua,Iraila,Urria,Azaroa,Abendua",
+months_short:"Urt,Ots,Mar,Api,Mai,Eka,Uzt,Abu,Ira,Urr,Aza,Abe",
+day_long:"Igandea,Astelehena,Asteartea,Asteazkena,Osteguna,Ostirala,Larunbata,Igandea",
+day_short:"Iga,Ast,Aar,Aaz,Osg,Ost,Lar,Iga"
+},
+print:{
+print_desc:"Inprimatu"
+},
+preview:{
+preview_desc:"Aurrebista"
+},
+directionality:{
+ltr_desc:"Ezkerretik eskuinerako zentzua",
+rtl_desc:"Eskuinetik ezkerrerako zentzua"
+},
+layer:{
+insertlayer_desc:"Kapa berria sartu",
+forward_desc:"Aurrera",
+backward_desc:"Atzera",
+absolute_desc:"Posizio absolutua aldatu",
+content:"Kapa berria..."
+},
+save:{
+save_desc:"Gorde",
+cancel_desc:"Aldaketa guztiak deuseztu"
+},
+nonbreaking:{
+nonbreaking_desc:"Espazio karakterea sartu"
+},
+iespell:{
+iespell_desc:"Ortografia zuzentzailea",
+download:"Ez da 'ieSpell' aurktu. Orain instalatu nahi duzu?"
+},
+advhr:{
+advhr_desc:"Marra horizontala"
+},
+emotions:{
+emotions_desc:"Ikurrak"
+},
+searchreplace:{
+search_desc:"Bilatu",
+replace_desc:"Bilatu/Ordezkatu"
+},
+advimage:{
+image_desc:"Irudia sartu/aldatu"
+},
+advlink:{
+link_desc:"Lotura sartu/aldatu"
+},
+xhtmlxtras:{
+cite_desc:"Zita",
+abbr_desc:"Laburdura",
+acronym_desc:"Akronimoa",
+del_desc:"Ezabatuta",
+ins_desc:"Sartuta",
+attribs_desc:"Atributuak gehitu/aldatu"
+},
+style:{
+desc:"CSS estiloa aldatu"
+},
+paste:{
+paste_text_desc:"Testu arrunt gisa itsatsi",
+paste_word_desc:"Wordeko testua itsatsi",
+selectall_desc:"Hautatu dena"
+},
+paste_dlg:{
+text_title:"Erabili CTRL+V zure teklatuan testua itsasteko.",
+text_linebreaks:"Lerro saltoak mantendu",
+word_title:"Erabili CTRL+V zure teklatuan testua itsasteko."
+},
+table:{
+desc:"Taula berria sartu",
+row_before_desc:"Lerroa sartu (aurretik)",
+row_after_desc:"Lerroa sartu (ondoren)",
+delete_row_desc:"Lerroa ezabatu",
+col_before_desc:"Zutabea sartu (aurretik)",
+col_after_desc:"Zutabea sartu (ondoren)",
+delete_col_desc:"Zutabea ezabatu",
+split_cells_desc:"Zeldak banatu",
+merge_cells_desc:"Zeldak batu",
+row_desc:"Lerroaren propietateak",
+cell_desc:"Zeldaren propietateak",
+props_desc:"Taularen propietateak",
+paste_row_before_desc:"Lerroak itsatsi (aurretik)",
+paste_row_after_desc:"Lerroak itsatsi (ondoren)",
+cut_row_desc:"Lerroa ebaki",
+copy_row_desc:"Lerroa kopiatu",
+del:"Taula ezabatu",
+row:"Lerroa",
+col:"Zutabea",
+cell:"Zelda"
+},
+autosave:{
+unload_msg:"Orrialdetik irteten bazara, egindako aldaketak galdu egingo dira."
+},
+fullscreen:{
+desc:"Orrialde osoko modura aldatu"
+},
+media:{
+desc:"Itsatsitako kodea sartu/aldatu",
+edit:"Itsatsitako kodea aldatu"
+},
+fullpage:{
+desc:"Dokumentuaren propietateak"
+},
+template:{
+desc:"Aurredefinitutako txantiloiaren edukia sartu"
+},
+visualchars:{
+desc:"ON/OFF kontrol karaktereak."
+},
+spellchecker:{
+desc:"Ortografia zuzentzailera aldatu",
+menu:"Zuzentzailearen konfigurazioa",
+ignore_word:"Baztertu",
+ignore_words:"Guztia baztertu",
+langs:"Hizkuntzak",
+wait:"Itxaron...",
+sug:"Iradokizunak",
+no_sug:"Iradokizunik ez",
+no_mpell:"Ez da akatsik aurkitu."
+},
+pagebreak:{
+desc:"Orrialde bukaera sartu"
+}}});
