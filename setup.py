6bf57b82d6db7724234c5d781b382a89f42617f2
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2009-11-18 16:27:03 +0000

    Update versions, put dates in changelogs, remove dev tag.  Oh we love the tag dance.
    svn path=/Products.TinyMCE/trunk/; revision=102635

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 import sys
 
-version = '1.1rc7'
+version = '1.1rc8'
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
     requires = ['simplejson']
