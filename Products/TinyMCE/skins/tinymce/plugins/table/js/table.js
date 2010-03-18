602034c1a19ce2ef71d79bf807af2478d955ca47
ristow <ristow@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-18 09:39:29 +0000

    - changed table plug-ins. Included option to insert the table header at tiny_mce_init.js (tables_firstline_th: true/false)  - js fix for ie
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@113363 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/TinyMCE/skins/tinymce/plugins/table/js/table.js
+++ Products/TinyMCE/skins/tinymce/plugins/table/js/table.js
@@ -8,7 +8,11 @@ function insertTable() {
 	var cols = 2, rows = 2, border = 0, cellpadding = -1, cellspacing = -1, align, width, height, className, caption, frame, rules;
 	var html = '', capEl, elm;
 	var cellLimit, rowLimit, colLimit;
-
+	if (tinyMCEPopup.getParam('table_firstline_th')) {
+		line_tag = "th";
+	} else {
+		line_tag = "td";
+	}
 	tinyMCEPopup.restoreSelection();
 
 	if (!AutoValidator.validate(formObj)) {
@@ -196,10 +200,11 @@ function insertTable() {
 
 		for (var x=0; x<cols; x++) {
 			if (!tinymce.isIE)
-				html += '<td><br mce_bogus="1"/></td>';
+				html += '<'+line_tag+'><br mce_bogus="1"/></'+line_tag+'>';
 			else
-				html += '<td></td>';
+				html += '<'+line_tag+'></'+line_tag+'>';
 		}
+		line_tag = "td";
 
 		html += "</tr>";
 	}
