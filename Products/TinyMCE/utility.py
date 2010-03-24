167f47bbd6d9c237c52f6b1c9bc69bb94e235517
erral <erral@localhost>
2010-03-24 11:02:11 +0000

    add basque language support for tinymce in Plone
    svn path=/Products.TinyMCE/trunk/; revision=113897

--- Products/TinyMCE/utility.py
+++ Products/TinyMCE/utility.py
@@ -716,9 +716,9 @@ class TinyMCE(SimpleItem):
             results['livesearch'] = False
 
         AVAILABLE_LANGUAGES = set(
-           'ar bs ch da el es fa fr he hu ii it ko lv ms nl pl ro sc si sl sr '
-           'tr tw vi bg ca cs de en et fi gl hr ia is ja lt mk nb nn pt ru se '
-           'sk sq sv tt uk zh'.split())
+           'ar bs ch da el es eu fa fr he hu ii it ko lv ms nl pl ro sc si sl '
+           'sr tr tw vi bg ca cs de en et fi gl hr ia is ja lt mk nb nn pt ru '
+           'se sk sq sv tt uk zh eu'.split())
 
         if context.REQUEST.has_key('LANGUAGE'):
             results['language'] = context.REQUEST.LANGUAGE[:2]
