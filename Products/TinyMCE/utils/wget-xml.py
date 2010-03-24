1294f4c3f6e3cae5ad060e2064983aee367eefbe
Vincent Fretin <vincent.fretin@gmail.com>
2010-03-24 13:49:00 +0000

    Added more languages (71 languages from XML files+pt_BR (but the tinymce domain is not picked up for me))
    svn path=/Products.TinyMCE/trunk/; revision=113908

--- Products/TinyMCE/utils/wget-xml.py
+++ Products/TinyMCE/utils/wget-xml.py
@@ -13,9 +13,9 @@ except ImportError:
 __WGET = os.environ.get('WGET', 'wget')
 
 AVAILABLE_LANGUAGES = set(
-    'ar bs ch da el es fa fr he hu ii it lv ms nl pl ro sc si sl sr '
-    'tr tw vi bg ca cs de en et eu fi gl hr ia is ja lt mk nb nn pt ru se '
-    'sk sq sv tt uk'.split())
+    'sq ar hy az eu be bn nb bs br bg ca ch zh hr cs da dv nl en et fi fr gl '
+    'de el gu he hi hu is id ia it ja ko lv lt lb mk ms ml mn se no nn fa pl '
+    'pt ps ro ru sc sr ii si sk sl es sv ta tt te th tr tw uk ur cy vi zu'.split())
 
 def main():
 
