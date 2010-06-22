194065f824b7b5d1a80dd33d3a3b03c4d4140272
vincentfretin <vincentfretin@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-06-22 08:26:47 +0000

    update langs js as well
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@119554 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/TinyMCE/skins/tinymce/plugins/searchreplace/langs/zh_dlg.js
+++ Products/TinyMCE/skins/tinymce/plugins/searchreplace/langs/zh_dlg.js
@@ -1,16 +1,16 @@
 tinyMCE.addI18n('zh.searchreplace_dlg',{
-searchnext_desc:"\u518D\u641C\u7D22\u4E00\u6B21",
-notfound:"\u672A\u627E\u5230\u641C\u7D22\u9879\u3002 ",
-search_title:"\u641C\u7D22",
-replace_title:"\u641C\u7D22/\u66FF\u6362",
+searchnext_desc:"\u518D\u5BFB\u627E\u4E00\u6B21",
+notfound:"\u672A\u627E\u5230\u5BFB\u627E\u9879\u3002 ",
+search_title:"\u5BFB\u627E",
+replace_title:"\u5BFB\u627E/\u53D6\u4EE3",
 allreplaced:"\u66F4\u65B0\u5B8C\u6210\u3002 ",
-findwhat:"\u641C\u7D22\u5185\u5BB9",
-replacewith:"\u66FF\u6362\u6210",
-direction:"\u641C\u7D22\u65B9\u5411",
+findwhat:"\u5BFB\u627E\u5185\u5BB9",
+replacewith:"\u53D6\u4EE3\u6210",
+direction:"\u5BFB\u627E\u65B9\u5411",
 up:"\u5F80\u4E0A",
 down:"\u5F80\u4E0B",
 mcase:"\u533A\u5206\u5927\u5C0F\u5199",
 findnext:"\u4E0B\u4E00\u4E2A",
-replace:"\u66FF\u6362",
-replaceall:"\u5168\u90E8\u66FF\u6362"
+replace:"\u53D6\u4EE3",
+replaceall:"\u5168\u90E8\u53D6\u4EE3"
 });
