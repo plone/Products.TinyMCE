15e7d4bf465419216bfcfd3b0fe6610198dc298a
David Glick <davidglick@groundwire.org>
2010-07-23 21:28:37 +0000

    fix the broken 'paste text' dialog
    svn path=/Products.TinyMCE/trunk/; revision=121780

--- Products/TinyMCE/skins/tinymce/plugins/paste/js/pastetext.js
+++ Products/TinyMCE/skins/tinymce/plugins/paste/js/pastetext.js
@@ -5,15 +5,24 @@ var PasteTextDialog = {
 	},
 
 	insert : function() {
-		var h = tinyMCEPopup.dom.encode(document.getElementById('content').value), lines;
+	    // Plone fix: rename the textarea to avoid conflicting with the main #content div
+		var h = tinyMCEPopup.dom.encode(document.getElementById('mceTextPaste').value), lines;
 
-		// Convert linebreaks into paragraphs
+		// Convert linebreaks into <br/> tags
+		// Plone fix: turn double-linebreaks into paragraph tags
 		if (document.getElementById('linebreaks').checked) {
-			lines = h.split(/\r?\n/);
+			lines = h.split(/\r?\n\s*\r?\n/);
 			if (lines.length > 1) {
 				h = '';
 				tinymce.each(lines, function(row) {
 					h += '<p>' + row + '</p>';
+				});
+			}
+			lines = h.split(/\r?\n/);
+			if (lines.length > 1) {
+				h = '';
+				tinymce.each(lines, function(row) {
+					h += '<br/>' + row;
 				});
 			}
 		}
