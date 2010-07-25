cd2038c1ab4cb958670201b31fcc4a95e028369c
Roel Bruggink <roel+plone.org@fourdigits.nl>
2010-07-25 12:11:12 +0000

    Mark as BBB and cleanup.
    svn path=/Products.TinyMCE/trunk/; revision=121871

--- Products/TinyMCE/setuphandlers.py
+++ Products/TinyMCE/setuphandlers.py
@@ -90,23 +90,11 @@ def uninstall_mimetype_and_transforms(context):
     unregister_mimetype(context, text_tinymce_output_html)
     unregister_transform_policy(context, "text/x-html-safe")
 
-def fix_styles_plone3(context):
-    portal_tinymce = getToolByName(context, 'portal_tinymce')
-    portal_migration = getToolByName(context, 'portal_migration')
-
-    use_tiny_mce_plone3_styles = False
-    if hasattr(portal_migration, 'getInstanceVersionTuple'):
-        major_version = portal_migration.getInstanceVersionTuple()[0]
-        if major_version == 3:
-            use_tiny_mce_plone3_styles = True
-    portal_tinymce.use_tiny_mce_plone3_styles = use_tiny_mce_plone3_styles
-
 def importVarious(context):
     if context.readDataFile('portal-tinymce.txt') is None:
         return
     site = context.getSite()
     add_editor(site)
-    fix_styles_plone3(site)
 
 def unregisterUtility(context):
     my_utility = getUtility(ITinyMCE)
