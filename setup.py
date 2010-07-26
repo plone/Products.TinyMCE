8acacc06fb929e7a739e68cdb949e7bfcac3f9c1
Roel Bruggink <roel+plone.org@fourdigits.nl>
2010-07-26 00:07:52 +0000

    Bump to 1.1.2dev
    svn path=/Products.TinyMCE/trunk/; revision=121906

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 import sys
 
-version = '1.1.1'
+version = '1.1.2dev'
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
     requires = ['simplejson']
