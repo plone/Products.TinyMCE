1dbf0409ac16d13c1a68a70aaee7562130253eb4
Laurence Rowe <l@lrowe.co.uk>
2011-02-10 17:29:30 +0000

    TinyMCE now works with Dexterity images. - Use @@images view for image scales.   [elro]
    - Get anchor names from listAnchorNames method of @@content_anchors.
      [elro]
    
    svn path=/Products.TinyMCE/trunk/; revision=231903

---
+++ Products/TinyMCE/browser/atanchors.py
@@ -0,0 +1,20 @@
+from Products.Five.browser import BrowserView
+from Products.TinyMCE.browser.interfaces.anchors import IAnchorView
+# Not available in xml.etree
+from elementtree import HTMLTreeBuilder
+from zope.interface import implements
+
+class ATAnchorView(BrowserView):
+    implements(IAnchorView)
+
+    def listAnchorNames(self):
+        """Return a list of Anchor names"""
+        results = []
+        tree = HTMLTreeBuilder.TreeBuilder()
+        tree.feed('<root>%s</root>' % self.context.getPrimaryField().getAccessor(self.context)())
+        rootnode = tree.close()
+        for x in rootnode.getiterator():
+            if x.tag == "a":
+                if "name" in x.keys():
+                    results.append(x.attrib['name'])
+        return results
