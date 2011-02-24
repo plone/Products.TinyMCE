2d48c46052e3870aa42b9e6da92c34024c1934f7
jaroel <jaroel@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-02-24 12:57:22 +0000

    Back to development: 1.2.4
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@232593 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 import sys
 
-version = '1.2.3'
+version = '1.2.4dev'
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
     requires = ['simplejson']
