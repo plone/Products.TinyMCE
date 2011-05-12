85f7bf24e8f75b41cfff43f05f190b790bb04c3b
Hanno Schlichting <hanno@hannosch.eu>
2011-05-12 09:39:51 +0000

    Merged /Products.TinyMCE/branches/1.1:r238382
    svn path=/Products.TinyMCE/trunk/; revision=239167

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 import sys
 
-version = '1.2.5dev'
+version = '1.2.5'
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
     requires = ['simplejson']
@@ -16,11 +16,9 @@ setup(name='Products.TinyMCE',
                        open(os.path.join("docs", "INSTALL.txt")).read() + "\n\n" +
                        open("CHANGES.txt").read() + "\n\n" +
                        open(os.path.join("docs", "CONTRIBUTORS.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='editor editors tinymce wysiwyg',
       author='Four Digits',
