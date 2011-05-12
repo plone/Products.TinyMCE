5ae518a8a46a0acafa99f86098dee4c8a4ff6953
Hanno Schlichting <hanno@hannosch.eu>
2011-05-12 09:41:26 +0000

    vb
    svn path=/Products.TinyMCE/trunk/; revision=239170

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 import sys
 
-version = '1.2.5'
+version = '1.2.6'
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
     requires = ['simplejson']
