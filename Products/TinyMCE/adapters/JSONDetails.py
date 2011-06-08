86fac36de989898d08f3af6a49bd4dc207af338a
maurits <maurits@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-06-08 14:11:59 +0000

    Nuke trailing white space
    git-svn-id: https://svn.plone.org/svn/collective/Products.TinyMCE/trunk@240643 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/TinyMCE/adapters/JSONDetails.py
+++ Products/TinyMCE/adapters/JSONDetails.py
@@ -64,12 +64,12 @@ class JSONDetails(object):
         results.update(self.additionalDetails())
 
         return json.dumps(results)
-    
+
     def additionalDetails(self):
         """Hook to allow subclasses to supplement or override the default set of results
         """
         return {}
-    
+
     def _getPloneUrl(self):
         """Return the URL corresponding to the root of the Plone site."""
         portal_url = getToolByName(self.context, 'portal_url')
