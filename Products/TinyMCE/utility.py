3bef93211616b62167000d94cfcbd28e1ef888ef
David Glick <davidglick@groundwire.org>
2011-07-26 23:21:23 +0000

    support plone.app.textfield
    svn path=/Products.TinyMCE/trunk/; revision=242843

--- Products/TinyMCE/utility.py
+++ Products/TinyMCE/utility.py
@@ -580,12 +580,20 @@ class TinyMCE(SimpleItem):
     def getContentType(self, object=None, fieldname=None):
         context = aq_base(object)
         if IBaseObject.providedBy(context):
+            # support Archetypes fields
             if fieldname is None:
                 field = context.getPrimaryField()
             else:
                 field = context.getField(fieldname) or getattr(context, fieldname, None)
             if field and hasattr(aq_base(field), 'getContentType'):
                 return field.getContentType(context)
+        elif '.widgets.' in fieldname:
+            # support plone.app.textfield RichTextValues
+            fieldname = fieldname.split('.widgets.')[-1]
+            field = getattr(context, fieldname, None)
+            mimetype = getattr(field, 'mimeType', None)
+            if mimetype is not None:
+                return mimetype
         return 'text/html'
 
     security.declareProtected('View', 'getConfiguration')
