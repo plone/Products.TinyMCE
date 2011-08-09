96d7415987bb1e2dc1283d02e8648fef85499fe7
Thomas Desvenain <thomas.desvenain@gmail.com>
2011-08-09 14:45:14 +0000

    When user tries to upload an image,   if image type in content types registry is not allowed in current folder,   TinyMCE tries to add an image of an other content type   selected as an image type in TinyMCE settings.
    svn path=/Products.TinyMCE/trunk/; revision=243269

--- Products/TinyMCE/adapters/Upload.py
+++ Products/TinyMCE/adapters/Upload.py
@@ -1,4 +1,5 @@
 from Acquisition import aq_inner
+from AccessControl.unauthorized import Unauthorized
 from zope.interface import implements
 from zope.component import getUtility
 
@@ -76,20 +77,31 @@ class Upload(object):
         context = self.context
         request = context.REQUEST
         ctr_tool = getToolByName(self.context, 'content_type_registry')
-        id = request['uploadfile'].filename
+        utility = getUtility(ITinyMCE)
 
+        id = request['uploadfile'].filename
         content_type = request['uploadfile'].headers["Content-Type"]
         typename = ctr_tool.findTypeName(id, content_type, "")
 
         # Permission checks based on code by Danny Bloemendaal
 
-        # 1) check if we are allowed to create an Image in folder
-        if not typename in [t.id for t in context.getAllowedTypes()]:
-            return self.errorMessage(_("Not allowed to upload a file of this type to this folder"))
-
-        # 2) check if the current user has permissions to add stuff
+        # 1) check if the current user has permissions to add stuff
         if not context.portal_membership.checkPermission('Add portal content', context):
             return self.errorMessage(_("You do not have permission to upload files in this folder"))
+
+        # 2) check image types uploadable in folder.
+        #    priority is to content_type_registry image type
+        allowed_types = [t.id for t in context.getAllowedTypes()]
+        if typename in allowed_types:
+            uploadable_types = [typename]
+        else:
+            uploadable_types = []
+
+        if content_type.split('/')[0] == 'image':
+            image_portal_types = utility.imageobjects.split('\n')
+            uploadable_types += [t for t in image_portal_types
+                                    if t in allowed_types
+                                       and t not in uploadable_types]
 
         # Get an unused filename without path
         id = self.cleanupFilename(id)
@@ -97,16 +109,27 @@ class Upload(object):
         title = request['uploadtitle']
         description = request['uploaddescription']
 
-        newid = context.invokeFactory(type_name=typename, id=id)
+        for metatype in uploadable_types:
+            try:
+                newid = context.invokeFactory(type_name=metatype, id=id)
+                if newid is None or newid == '':
+                    newid = id
 
-        if newid is None or newid == '':
-            newid = id
+                obj = getattr(context, newid, None)
 
-        obj = getattr(context, newid, None)
+                # set primary field
+                pf = obj.getPrimaryField()
+                pf.set(obj, request['uploadfile'])
+                break
+
+            except ValueError:
+                continue
+        else:
+            return self.errorMessage(_("Not allowed to upload a file of this type to this folder"))
+
 
         # Set title + description.
         # Attempt to use Archetypes mutator if there is one, in case it uses a custom storage
-
         if title:
             try:
                 obj.setTitle(title)
@@ -119,18 +142,14 @@ class Upload(object):
             except AttributeError:
                 obj.description = description
 
-        # set primary field
-        pf = obj.getPrimaryField()
-        pf.set(obj, request['uploadfile'])
-
         if not obj:
             return self.errorMessage(_("Could not upload the file"))
 
         obj.reindexObject()
 
-        utility = getUtility(ITinyMCE)
         if utility.link_using_uids:
             return self.okMessage("resolveuid/%s" % (uuidFor(obj)))
+
         return self.okMessage("%s" % (obj.absolute_url()))
 
     def setDescription(self, description):
