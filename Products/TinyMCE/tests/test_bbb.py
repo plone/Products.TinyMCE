aac0d57f8f5f3a1098b45a70900a573419387bb3
Tom Gross <tom@toms-projekte.de>
2011-08-25 10:30:36 +0200

    added bbb-tests

---
+++ Products/TinyMCE/tests/test_bbb.py
@@ -0,0 +1,19 @@
+import unittest
+
+import zope.interface
+
+from Products.TinyMCE.bbb import implementedOrProvidedBy
+
+class TestObject(object):
+    """ A simple object """
+
+class ITestIface(zope.interface.Interface):
+    """ A simple interface """
+
+class BBBTests(unittest.TestCase):
+
+    def test_implementedOrProvidedBy(self):
+        myobj = TestObject()
+        self.assertFalse(implementedOrProvidedBy(ITestIface, myobj))
+        zope.interface.directlyProvides(myobj, ITestIface)
+        self.assertTrue(implementedOrProvidedBy(ITestIface, myobj))
