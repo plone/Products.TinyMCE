6a673146a759a13f1e511edf027bef584c2e55fc
Tom Gross <tom@toms-projekte.de>
2011-08-26 20:27:30 +0200

    use 'directlyProvides' to be compatible with Plone < 4.1

--- Products/TinyMCE/vocabularies.py
+++ Products/TinyMCE/vocabularies.py
@@ -1,23 +1,23 @@
 from plone.app.imaging.utils import getAllowedSizes
 from zope.component import getUtilitiesFor
-from zope.interface import provider
+from zope.interface import directlyProvides
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.interfaces import IContextSourceBinder
 
 
-@provider(IContextSourceBinder)
 def shortcuts_vocabulary(context):
     from Products.TinyMCE.interfaces.shortcut import ITinyMCEShortcut  # circular dependency
     terms = []
     for name, utility in getUtilitiesFor(ITinyMCEShortcut):
         terms.append(SimpleVocabulary.createTerm(name, str(name), utility.title))
     return SimpleVocabulary(terms)
+directlyProvides(shortcuts_vocabulary, IContextSourceBinder)
 
 
-@provider(IContextSourceBinder)
 def thumbnail_sizes_vocabulary(context):
     terms = []
     # TODO: we should query utility, but it's not certain it will be there
     for name, size in getAllowedSizes().iteritems():
         terms.append(SimpleVocabulary.createTerm(tuple((name,) + size), str(name), u"%s %s" % (name, size)))
     return SimpleVocabulary(terms)
+directlyProvides(thumbnail_sizes_vocabulary, IContextSourceBinder)
