b311ae985a044ae5b083599064a907640ee4c42f
Tom Gross <tom@toms-projekte.de>
2011-08-27 11:56:18 +0200

    fixed condition in upgrade_tinymce-script

--- upgrade_tinymce.sh
+++ upgrade_tinymce.sh
@@ -33,7 +33,7 @@ find $tinymce_root -name "*_src.js" -delete
 
 echo "*** Updating language files ..."
 cd $product_root/utils/
-if [  -d $product_root/utils/xml ]
+if [ ! -d $product_root/utils/xml ]
 then
     echo "*** Fetching translations ... ***"
     python wget-xml.py
