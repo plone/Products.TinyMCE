3973e977145105c03d540e000263b501150c764c
Tom Gross <tom@toms-projekte.de>
2011-11-15 10:37:00 +0100

    toggle display of editor for different text formats

--- Products/TinyMCE/browser/tiny_mce_gzip.js
+++ Products/TinyMCE/browser/tiny_mce_gzip.js
@@ -6,14 +6,29 @@
  * Copyright (c) 2008 Jason Davies
  * Licensed under the terms of the MIT License (see LICENSE.txt)
  *
- * Usage: copy this file into the same directory as tiny_mce.js and change
- * settings.page_name below to match your tinymce installation as appropriate.
  */
 
 jQuery(function($) {
   <tal:loop repeat="conf options/tinymce_json_config">
-    $('textarea#<tal:fieldname tal:replace="conf/fieldname" />').tinymce(
+    $('#<tal:fieldname tal:replace="conf/fieldname" />').tinymce(
       <tal:url tal:replace="structure conf/config" />);
+
+    var text_format = $('#<tal:fieldname tal:replace="conf/fieldname" />_text_format');
+  
+    text_format.change( function() {
+
+        var editor = $('#<tal:fieldname tal:replace="conf/fieldname" />').tinymce()
+
+        var mimetype = $('option:selected' ,this).val();
+        if (mimetype.indexOf('html') === -1) {  
+//        XXX don't hide editor. for some reason the field value
+//        does not get extracted any more.
+//        editor.hide()
+        } else {
+            editor.show()
+        }
+
+    });
   </tal:loop>
 
     // set Text Format dropdown untabbable for better UX
