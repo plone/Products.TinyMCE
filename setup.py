8b3d149f2a493496a624f8d52939a614cbe625a8
Maurits van Rees <maurits@vanrees.org>
2011-12-21 11:37:30 +0100

    Back to development: 1.1.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 import sys
 
-version = '1.1.12'
+version = '1.1.13dev'
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
     requires = ['simplejson']
