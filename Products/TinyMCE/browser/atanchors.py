56cbab8c5f0a85b7c9529ba5f640a67a159519de
Tom Gross <itconsense@gmail.com>
2012-05-28 15:15:32 +0200

    fixed and optimized retrival of anchors (etree & lxml)

--- Products/TinyMCE/browser/atanchors.py
+++ Products/TinyMCE/browser/atanchors.py
@@ -1,19 +1,27 @@
+from ZODB.POSException import ConflictError
 from Products.Five.browser import BrowserView
 from Products.TinyMCE.browser.interfaces.anchors import IAnchorView
 from zope.interface import implements
 
 try:
-    from lxml.html import parse
+    from lxml.html import fromstring
+    SEARCHPATTERN = "a"
 except ImportError:
-    from elementtree.HTMLTreeBuilder import parse
+    from elementtree import HTMLTreeBuilder
 
+    def fromstring(text):
+        parser = HTMLTreeBuilder.TreeBuilder()
+        text = '<root>%s</root>' % text
+        parser.feed(text)
+        return parser.close()
+
+    SEARCHPATTERN = "*/a"
 
 class ATAnchorView(BrowserView):
     implements(IAnchorView)
 
     def listAnchorNames(self, fieldname=None):
         """Return a list of Anchor names"""
-        results = []
         if not fieldname:
             field = self.context.getPrimaryField()
         else:
@@ -21,11 +29,10 @@ class ATAnchorView(BrowserView):
 
         htmlsnippet = field.getAccessor(self.context)()
         try:
-            tree = parse('<root>%s</root>' % htmlsnippet)
-        except:
-            return results
-        for x in tree.getroot().getiterator():
-            if x.tag == "a":
-                if "name" in x.keys():
-                    results.append(x.attrib['name'])
-        return results
+            tree = fromstring(htmlsnippet)
+        except ConflictError:
+            raise
+        except Exception:
+            return []
+        return [anchor.get('name') for anchor in tree.findall(SEARCHPATTERN)
+                if "name" in anchor.keys()]
