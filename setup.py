f75c096de75b06a37460ad80f47739facb9ff741
Maurits van Rees <maurits@vanrees.org>
2012-08-18 18:37:23 +0200

    Declare dependency on zope.app.component.

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(name='Products.TinyMCE',
           'plone.app.imaging>=1.0.2',
           'plone.outputfilters',
           'zope.app.content',
+          'zope.app.component',
       ] + requires,
       extras_require={
             'test': ['plone.app.testing', 'unittest2'],
