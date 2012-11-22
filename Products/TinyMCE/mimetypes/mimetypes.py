2a61b35b15a2a647c1fdf643126ee1bc5d0cc9b2
Tom Gross <itconsense@gmail.com>
2012-11-22 08:45:27 +0100

    readded mimetype, it breaks the mimetype-registry otherwise

---
+++ Products/TinyMCE/mimetypes/mimetypes.py
@@ -0,0 +1,7 @@
+from Products.MimetypesRegistry.MimeTypeItem import MimeTypeItem
+
+
+class text_tinymce_output_html(MimeTypeItem):
+    __name__ = "TinyMCE Output HTML"
+    mimetypes = ('text/x-tinymce-output-html',)
+    binary = 0
