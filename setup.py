5179b1c949382e4cde92529218b2c04e9b0c3a24
Maurits van Rees <maurits@vanrees.org>
2015-06-05 14:56:42 +0200

    setup.py: Python 2.6 and 2.7 classifier

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ setup(name='Products.TinyMCE',
     classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
     ],
     keywords='editor editors tinymce wysiwyg',
     author='Four Digits',
