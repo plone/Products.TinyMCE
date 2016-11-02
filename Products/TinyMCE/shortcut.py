7eb0615b2d033a95d43fdf47c3ae312ed2b2bd6f
Luca Fabbri <luca@keul.it>
2016-11-02 12:46:24 +0100

    Fix to current folder shortcut
    Preventing current folder shortcut to link to a temporary object; fallback to the container

--- Products/TinyMCE/shortcut.py
+++ Products/TinyMCE/shortcut.py
@@ -1,6 +1,8 @@
 """Shortcuts implementation for Plone"""
 
+from Acquisition import aq_parent, aq_inner
 from Products.TinyMCE.interfaces.utility import _
+from Products.CMFCore.utils import getToolByName
 from zope.i18n import translate
 
 
@@ -9,10 +11,16 @@ class CurrentFolderShortcut(object):
     title = _(u'Current Folder')
 
     def render(self, context):
+        portal_factory = getToolByName(context, 'portal_factory')
+        if portal_factory.isTemporary(context):
+            # Fix current folder URL on creation
+            url = aq_parent(aq_parent(aq_parent(aq_inner(context)))).absolute_url()
+        else:
+            url = context.absolute_url()
         return ["""
         <img src="img/folder_current.png" />
         <a id="currentfolder" href="%s">%s</a>
-        """ % (context.absolute_url(), translate(self.title, context=context.REQUEST))]
+        """ % (url, translate(self.title, context=context.REQUEST))]
 
 
 class HomeShortcut(object):
