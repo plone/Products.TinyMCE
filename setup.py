8d77d2076bf259b7b1ad4f1d7f4ce0dfc8e3893d
Maurits van Rees <maurits@vanrees.org>
2016-11-09 16:58:29 +0100

    Preparing release 1.3.24
    [ci skip]

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='Products.TinyMCE',
-    version='1.3.24.dev0',
+    version='1.3.24',
     description=("Adds support for TinyMCE, a platform independent "
                  "web based Javascript HTML WYSIWYG editor, to Plone."),
     long_description=long_description,
